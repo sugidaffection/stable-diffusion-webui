5d4f61dfdd20a1a46df797f0953766dba99d8ea5
Sugi <developer.sugiono@gmail.com>
2025-06-30 17:55:59 +0700

    update requirements and command line arguments for improved compatibility and performance

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--xformers --no-half-vae --no-half
+set COMMANDLINE_ARGS=--xformers --no-half-vae --no-half --opt-split-attention --precision full
 
 call webui.bat
